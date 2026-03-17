cb538ed021f4a9ff53ff1af4dee9afb22b0e5f74
Xena <wuxianxian0920@gmail.com>
2026-03-17 22:29:49 +0800

    提交人Xena016 挑战尝试闯关到Day16 (#1094)
    * Add local Solidity backup code into Xena folder
    
    * Add Solidity day3 PollStation.sol
    
    * Add day4 Solidity code
    
    * Add day5 solidity code
    
    * Add day6 solidity code
    
    * Add day7 solidity code
    
    * day8
    
    * add day9
    
    * add day10
    
    * add day11
    
    * day12
    
    * day13-day14
    
    * day15
    
    * day16

---
+++ Xena/day16-WeaponPluginStore.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract WeaponPlugin{
+    mapping(address => string) public equippedWeapon;
+
+    function setWeapon(address user, string memory weapon) public {
+        equippedWeapon[user] = weapon;
+    }
+
+    function getWeapon(address user) public view returns(string memory){
+        return equippedWeapon[user];
+    }
+}
